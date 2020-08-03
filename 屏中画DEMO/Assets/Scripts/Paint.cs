c7cfc9a0a2a7d6a3547bf2a0458fffc93d11a0b2
currybeefer <544696166@qq.com>
2020-08-03 23:17:54 +0800

    2020年8月3日23:17:23
    增加了旋转和移动调换位置的功能

---
+++ 屏中画DEMO/Assets/Scripts/Paint.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paint : MonoBehaviour
+{
+    private Transform m_Transform;//屏风本身的Transform组件
+    public Transform groundTransform;//实际的地图的Transform组件
+    public Transform cameraTransform;//每一块地图的摄像机的Transform组件
+    public float speed = 100;
+    private bool isClicked = false;
+    private Animation m_Animation;
+    public int state;
+    private Vector3 originPosition;//保存在移动位置之前时的位置
+    void Start()
+    {
+        m_Transform = gameObject.GetComponent<Transform>();
+        m_Animation = gameObject.GetComponent<Animation>();
+        state = 1;
+    }
+
+    void Update()
+    {
+        if (isClicked)//旋转的方法
+        {
+            if (state == 1)
+            {
+                isClicked = false;
+                m_Animation.clip = m_Animation.GetClip("Rotate1");
+                m_Animation.Play();
+                Invoke("RotateGround", 0.5f);
+                state = 2;
+            }
+        }
+        if (isClicked)
+        {
+            if (state == 2)
+            {
+                isClicked = false;
+                m_Animation.clip = m_Animation.GetClip("Rotate2");
+                m_Animation.Play();
+                Invoke("RotateGround", 0.5f);
+                state = 1;
+            }
+        }
+    }
+    void RotateGround()
+    {
+        groundTransform.eulerAngles += new Vector3(0, 180, 0);
+    }
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))//如果玩家点击鼠标右键就会旋转
+        {
+            isClicked = true;
+        }
+    }
+    IEnumerator OnMouseDown()
+    {
+        originPosition = m_Transform.position;
+        Vector3 temp = m_Transform.position;
+        temp.z -= 1.5f;
+        m_Transform.position = temp;
+        Vector3 screenSpace = Camera.main.WorldToScreenPoint(m_Transform.position);//三维物体坐标转屏幕坐标
+        //将鼠标屏幕坐标转为三维坐标，再计算物体位置与鼠标之间的距离
+        Vector3 offset = m_Transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, screenSpace.z));
+        while (Input.GetMouseButton(0))
+        {
+            Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, 0, screenSpace.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+            m_Transform.position = curPosition;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+    private void OnMouseUp()
+    {
+        Replace();//放下时发出射线，互换地图块位置
+    }
+    private void Replace()
+    {
+        Ray ray = new Ray(m_Transform.position, Vector3.forward);
+        RaycastHit hit;
+        bool isCollider = Physics.Raycast(ray, out hit);
+        if (isCollider)
+        {
+            if(hit.collider.gameObject.tag=="Screen")
+            {
+                //互换屏风位置
+                Vector3 temp = hit.collider.gameObject.transform.position;
+                hit.collider.gameObject.transform.position = originPosition;
+                m_Transform.position = temp;
+                //互换地图块位置
+                Vector3 gTemp = hit.collider.gameObject.GetComponent<Paint>().groundTransform.position;
+                hit.collider.gameObject.GetComponent<Paint>().groundTransform.position = groundTransform.position;
+                groundTransform.position = gTemp;
+                //互换摄像机位置
+                Vector3 cTemp = hit.collider.gameObject.GetComponent<Paint>().cameraTransform.position;
+                hit.collider.gameObject.GetComponent<Paint>().cameraTransform.position = cameraTransform.position;
+                cameraTransform.position = cTemp;
+            }
+        }
+    }
+}
---
+++ 屏中画DEMO/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform m_Transform;
+    void Start()
+    {
+        m_Transform = gameObject.GetComponent<Transform>();
+    }
+    
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_Transform.SetParent(collision.transform);
+    }
+}
